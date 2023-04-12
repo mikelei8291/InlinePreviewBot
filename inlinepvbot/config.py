b88b80c9ec8fedb1bc2dd8d8fe95618e88928bee
Mike Lei <mikelei8291@users.noreply.github.com>
2023-04-12 11:17:41 +0000

    refactor(config): add system config path to default POSIX config paths
    Namely `/etc/{__package__}`.
    Raise an error when the config file is not found in any one of the specified locations.

--- inlinepvbot/config.py
+++ inlinepvbot/config.py
@@ -1,25 +1,33 @@
 import os
-from os.path import expanduser, join
+from os.path import expanduser, isfile, join
 from typing import Any
 
 from yaml import safe_load
 
 
 class ConfigLoader:
+    _FILENAME = "config.yaml"
+
     def __init__(self, config_path: str | None = None):
         if not config_path:
             if os.name == "posix":
-                xdg_config_home = os.getenv("XDG_CONFIG_HOME") or expanduser("~/.config")
-                config_path = join(xdg_config_home, __package__)
+                paths = [join(path, __package__, self._FILENAME) for path in (
+                    os.getenv("XDG_CONFIG_HOME") or expanduser("~/.config"),  # user config
+                    "/etc"  # system config
+                )]
             elif os.name == "nt" and (appdata := os.getenv("appdata")):
-                config_path = join(appdata, __package__)
+                paths = [join(appdata, __package__, self._FILENAME)]
             else:
-                config_path = join(expanduser("~"), f".{__package__}")
+                paths = [join(expanduser("~"), f".{__package__}", self._FILENAME)]
+            try:
+                config_path = next(filter(isfile, paths))
+            except StopIteration:
+                raise RuntimeError(f"Config file not found in any one of the locations: {paths}")
         self.config_path = config_path
 
     def load(self) -> dict[str, Any]:
         try:
-            with open(join(self.config_path, "config.yaml"), encoding="utf-8") as f:
+            with open(self.config_path, encoding="utf-8") as f:
                 return safe_load(f)
         except OSError as e:
             raise RuntimeError(f"Cannot open config file: {e.filename}") from e
