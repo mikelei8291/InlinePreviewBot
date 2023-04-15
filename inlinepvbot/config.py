43bac215ff26d4698cf20df0cea26bfcc65e93f6
Mike Lei <mikelei8291@users.noreply.github.com>
2023-04-15 09:53:08 +0000

    refactor(config): use `pathlib.Path` for file path operations

--- inlinepvbot/config.py
+++ inlinepvbot/config.py
@@ -1,33 +1,34 @@
 import os
-from os.path import expanduser, isfile, join
+from pathlib import Path
 from typing import Any
 
+from telebot import logger
 from yaml import safe_load
 
 
 class ConfigLoader:
     _FILENAME = "config.yaml"
 
-    def __init__(self, config_path: str | None = None):
+    def __init__(self, config_path: Path | None = None):
         if not config_path:
             if os.name == "posix":
-                paths = [join(path, __package__, self._FILENAME) for path in (
-                    os.getenv("XDG_CONFIG_HOME") or expanduser("~/.config"),  # user config
+                paths = [Path(path).expanduser() / __package__ / self._FILENAME for path in (
+                    os.getenv("XDG_CONFIG_HOME") or "~/.config",  # user config
                     "/etc"  # system config
                 )]
             elif os.name == "nt" and (appdata := os.getenv("appdata")):
-                paths = [join(appdata, __package__, self._FILENAME)]
+                paths = [Path(appdata) / __package__ / self._FILENAME]
             else:
-                paths = [join(expanduser("~"), f".{__package__}", self._FILENAME)]
+                paths = [Path("~").expanduser() / f".{__package__}" / self._FILENAME]
             try:
-                config_path = next(filter(isfile, paths))
+                config_path = next(p for p in paths if p.is_file())
             except StopIteration:
-                raise RuntimeError(f"Config file not found in any one of the locations: {paths}")
+                raise RuntimeError(f"Config file not found in any one of the locations: {list(map(str, paths))}")
         self.config_path = config_path
 
     def load(self) -> dict[str, Any]:
         try:
-            with open(self.config_path, encoding="utf-8") as f:
+            with self.config_path.open(encoding="utf-8") as f:
                 return safe_load(f)
         except OSError as e:
             raise RuntimeError(f"Cannot open config file: {e.filename}") from e
