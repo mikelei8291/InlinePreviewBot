48964f006cd4aac1d34529006bb76b42da85e488
Mike Lei <mikelei8291@users.noreply.github.com>
2023-04-20 04:23:49 +0800

    docs(bot): add documentation for the `InlinePreviewBot` class

--- inlinepvbot/bot.py
+++ inlinepvbot/bot.py
@@ -25,7 +25,24 @@ _EXTRACTORS: list[Extractor] = [getattr(extractor, _class) for _class in extract
 
 
 class InlinePreviewBot(AsyncTeleBot):
+    """The main class of the bot."""
+
     def __init__(self, *args, cache_time: int | None = None, config_path: Path | None = None, **kwargs):
+        """Initialize the bot with arguments.
+
+        `args` and `kwargs` are positional and keyword arguments of the `AsyncTeleBot` class, respectively.
+        Optionally, a `cache_time` can be specified and will be used in all `AsyncTeleBot.answer_inline_query()`
+        calls to set the cache time of the result on the API server. Also, the path to the configuration
+        file of the bot can be specified with the `config_path` argument, and the specified file will be
+        loaded as the configuration, instead of using paths pre-defined in the `ConfigLoader` class.
+
+        - args: Positional arguments to the `AsyncTeleBot`.
+        - cache_time: The time in seconds that the result of the inline query may be cached
+          on the API server. Default value of the Telegram official API server is 300 seconds.
+        - config_path: The path to the configuration file of the bot. If not specified, a set of
+          platform-specific paths will be used.
+        - kwargs: Keyword arguments to the `AsyncTeleBot`.
+        """
         super().__init__(*args, **kwargs)
         self.cache_time = cache_time
         self.config = ConfigLoader(config_path).load()
@@ -35,15 +52,38 @@ class InlinePreviewBot(AsyncTeleBot):
         self.register_inline_handler(self.inline_query_default, None)
 
     def inline_url_filter(self, query: InlineQuery) -> bool:
+        """Test if inputs from inline queries are valid URLs.
+
+        - query: The `InlineQuery` object to be tested.
+        - return: `True` if the query string is a valid URL, `False` otherwise.
+        """
         try:
             return parse_obj_as(HttpUrl, query.query).tld is not None
         except ValidationError:
             return False
 
     async def messages(self, message: Message) -> None:
+        """Handler for text messages.
+
+        Currently, this bot will reply to any text messages with a prompt indicating that the bot can
+        only be used in inline mode.
+
+        - message: The message to be handled.
+        """
         await self.send_message(message.chat.id, "This bot can only be used in inline mode")
 
     async def inline_url(self, query: InlineQuery) -> None:
+        """Handler for inline queries that are valid URLs.
+
+        The query string will be tested by extractors in the `_EXTRACTORS` list.
+        If any one of the extractors in the list successfully extracted data from the URL, the result would
+        be generated from the "url" message template specified in the configuration file.
+        If no extractor was able to extract data from the URL, the user would be prompted that the URL is
+        unsupported, and the result would be generated from the "unsupported" message template specified in
+        the configuration file.
+
+        - query: The inline query to be handled.
+        """
         data = {"url": query.query}
         for url_extractor in _EXTRACTORS:
             if result := await url_extractor.extract(query.query):
@@ -54,6 +94,14 @@ class InlinePreviewBot(AsyncTeleBot):
         )
 
     async def inline_query_default(self, query: InlineQuery) -> None:
+        """Handler for inline queries that are empty strings or invalid URLs.
+
+        A default response will be generated based on whether the query string is empty or not.
+        If the query string is empty, the result will be "No URL found". Otherwise, the result will be
+        "Invalid URL".
+
+        - query: The inline query to be handled.
+        """
         response_text = "Invalid URL" if query.query else "No URL found"
         await self.answer_inline_query(query.id, [
             InlineQueryResultArticle(
