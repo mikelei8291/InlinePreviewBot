1952271cc2b01ddde2ba4c707195e3a69cafecf8
Mike Lei <mikelei8291@users.noreply.github.com>
2023-05-30 07:09:17 +0800

    refactor(exception): improve logging of exceptions
    Improve exception messages in "error" level logs.
    Log stack traces of exceptions in "debug" level logs.

--- inlinepvbot/exception.py
+++ inlinepvbot/exception.py
@@ -1,11 +1,14 @@
+import traceback
+
 import telebot
 from telebot import logger
 from telebot.asyncio_helper import ApiException
 
 
 class ExceptionHandler(telebot.ExceptionHandler):
-    def handle(self, exception):
+    def handle(self, exception) -> None:
         if isinstance(exception, ApiException):
             if 400 <= exception.error_code < 500:
                 raise RuntimeError(exception.description) from exception
-        logger.error(exception)
+        logger.error(repr(exception))
+        logger.debug(traceback.format_exc())
